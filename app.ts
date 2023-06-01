ad2bd1de01b1f472410c93aec29fd773c213368e
Shayan Biswas <123059890+sbrb@users.noreply.github.com>
2023-06-02 00:47:33 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,47 @@
+// //1
+// function cal(n1: number, n2: number) {
+//     return n1 + n2;
+// }
+// console.log(cal(2,3));
+
+
+// //2
+// function mul(num:number[]) {
+//     return num.map(n => {
+//         return n * n;
+//     });
+// }
+// console.log(mul([1, 2, 3, 4, 5]));
+
+// //3
+// const user = {
+//     fname: 'John',
+//     lname: 'Doe',
+//     age: 30,
+//     role:'admin'
+// }
+
+// console.log(user.fname);
+
+// //4
+// //type alias
+// type User = {
+//     fname: string,
+//     lname: string,
+//     age: number,
+//     role?: string
+// }
+
+// function login(userData: User):User{
+//     return userData;
+// }
+
+// const user = {
+//     fname: 'John',
+//     lname: 'Doe',
+//     age: 77,
+// }
+// console.log(login(user));
+
+
+// //5
